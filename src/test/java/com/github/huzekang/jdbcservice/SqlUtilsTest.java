65c2f4760bfe8a26ec4dc05ab203f90def765c86
huzekang <1040080742@qq.com>
2019-11-09 15:35:27 +0800

    完成根据sql获取数据

--- src/test/java/com/github/huzekang/jdbcservice/SqlUtilsTest.java
+++ src/test/java/com/github/huzekang/jdbcservice/SqlUtilsTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: jdbc-service
@@ -147,4 +148,55 @@ public class SqlUtilsTest {
 
 
     }
+
+    /**
+     * 获取sql查询的分页数据
+     * @throws Exception
+     */
+    @Test
+    public void getSqlDataPage() throws Exception {
+        String sourceCreateJson = "{\"id\":0,\"name\":\"test\",\"type\":\"jdbc\",\"description\":\"\",\"config\":{\"username\":\"root\",\"password\":\"eWJmP7yvpccHCtmVb61Gxl2XLzIrRgmT\",\"url\":\"jdbc:mysql://localhost?serverTimezone=UTC\",\"parameters\":\"\",\"ext\":false,\"version\":\"\"},\"projectId\":17}";
+        SourceCreate sourceCreate = JSONUtil.toBean(sourceCreateJson,SourceCreate.class);
+        SourceConfig config = sourceCreate.getConfig();
+
+        // 根据jdbc连接配置获取一个组装好的sqlUtils
+        SqlUtils assemblySqlUtils = sqlUtils.init(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword(),
+                config.getVersion(),
+                config.isExt()
+        );
+
+        PaginateWithQueryColumns paginateWithQueryColumns = assemblySqlUtils
+                .query4Paginate("select id ,user_id ,chart_name from yiboard.chart",
+                2, 4, 100, 10, null);
+        System.out.println(paginateWithQueryColumns.getColumns());
+        System.out.println(paginateWithQueryColumns.getResultList() );
+    }
+
+    /**
+     * 获取sql查询的所有数据
+     * @throws Exception
+     */
+    @Test
+    public void getSqlData() throws Exception {
+        String sourceCreateJson = "{\"id\":0,\"name\":\"test\",\"type\":\"jdbc\",\"description\":\"\",\"config\":{\"username\":\"root\",\"password\":\"eWJmP7yvpccHCtmVb61Gxl2XLzIrRgmT\",\"url\":\"jdbc:mysql://localhost?serverTimezone=UTC\",\"parameters\":\"\",\"ext\":false,\"version\":\"\"},\"projectId\":17}";
+        SourceCreate sourceCreate = JSONUtil.toBean(sourceCreateJson,SourceCreate.class);
+        SourceConfig config = sourceCreate.getConfig();
+
+        // 根据jdbc连接配置获取一个组装好的sqlUtils
+        SqlUtils assemblySqlUtils = sqlUtils.init(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword(),
+                config.getVersion(),
+                config.isExt()
+        );
+
+        List<Map<String, Object>> mapList = assemblySqlUtils
+                .query4List("select id ,user_id ,chart_name from yiboard.chart", -1);
+        System.out.println(mapList);
+    }
+
 }
